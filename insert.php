da95f07b86d8e8cfe4571cbfc2b60ac910335e8b
Lakshana <lakshravi23@gmail.com>
2021-05-26 12:49:33 +0530

    retrieves info from db and displays; css for html table pending

--- insert.php
+++ insert.php
@@ -12,16 +12,16 @@ if ($conn->connect_error) {
 } 
 
 $docid=$_POST["did"];
-echo $docid;
+//echo $docid;
 
 $docname=$_POST["dname"];
-echo $docname;
+//echo $docname;
 
 $ddate=$_POST["day"];
-echo $ddate;
+//echo $ddate;
 
 $dtime=$_POST["dtime"];
-echo $dtime;
+//echo $dtime;
 
 
 $sql = "INSERT INTO addAppointment (id,name,date,Time)
@@ -34,4 +34,38 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
+?>
+
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "testdata";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $docid=$_POST["did"];
+  $sql = "SELECT date, Time FROM addAppointment WHERE id = '$docid' ";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+
+    // html table
+    echo "<table><tr><th>Date</th><th>Time</th></tr>";
+
+    //output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "<br>";
+      echo "<tr><td>".$row["date"]."</td><td>".$row["Time"]."</td></tr>";
+    }
+  } 
+  else {
+    echo "No results";
+  }
+  $conn->close();
 ?>
